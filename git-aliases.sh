acd7cce0e6bf8b1a876994a238de500ff884f5d5
Peter McGarrigle <pete@iMac.local>
2017-02-25 21:30:01 +0000

    added my git aliases

---
+++ git-aliases.sh
@@ -0,0 +1,13 @@
+
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add -A'
+alias gc='git commit -m'
+alias gg='git pull'
+alias gp='git push'
+alias co='git checkout'
+alias gr='git remote -v'
+alias gl='git log --graph --pretty=oneline --abbrev-commit'
+alias gk="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
+
