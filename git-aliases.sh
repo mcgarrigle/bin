9678fa512fb08f6ba827bbc82c846b7e4f41fbd8
Peter McGarrigle <pete@iMac.local>
2017-02-27 08:18:40 +0000

    updated $1

--- git-aliases.sh
+++ git-aliases.sh
@@ -7,7 +7,13 @@ alias gc='git commit -m'
 alias gg='git pull'
 alias gp='git push'
 alias co='git checkout'
+alias gd='git diff'
 alias gr='git remote -v'
 alias gl='git log --graph --pretty=oneline --abbrev-commit'
 alias gk="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
+
+function fix {
+  git add $1
+  git commit -m 'updated $1'
+}
 
