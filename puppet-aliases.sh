2054cdd6ea4a251cf96e901a8c7779025dd3e35c
Peter McGarrigle <peter.mcgarrigle@bt.com>
2018-11-07 18:05:35 +0000

    added autopdk

--- puppet-aliases.sh
+++ puppet-aliases.sh
@@ -3,3 +3,6 @@ alias pp='puppet apply --verbose ~/site.pp'
 alias pd='puppet apply --debug --verbose ~/site.pp'
 alias pl='puppet parser validate'
 
+function h {
+  hiera -dc /etc/puppetlabs/puppet/hiera.yaml "$1" ::trusted.certname=$2 ::fqdn=$2 environment=production
+}
