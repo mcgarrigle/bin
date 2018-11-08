cc6a25e9ba6d77d07189bb5a5518e459818380aa
Peter McGarrigle <peter.mcgarrigle@bt.com>
2018-11-08 11:09:30 +0000

    added pdk validate to autopdk

--- movein.sh
+++ movein.sh
@@ -1,9 +1,10 @@
 
-yum install -y vim git
+yum install -y vim git tmux
 
 git clone 'https://github.com/mcgarrigle/bin.git'
 
 echo -e '\n. bin/bashrc\n' >> ~/.bashrc
 
-ln -s bin/vimrc .vimrc
+ln -s bin/vimrc     .vimrc
+ln -s bin/tmux.conf .tmux.conf
 
