17dc779e4b956a08b5a0d8f53737687cd3a9ba89
Peter McGarrigle <peter.mcgarrigle@bt.com>
2018-11-14 12:11:18 +0000

    gl is in colour

--- git-aliases.sh
+++ git-aliases.sh
@@ -10,7 +10,8 @@ alias gp='git push'
 alias co='git checkout'
 alias gd='git diff'
 alias gr='git remote -v'
-alias gl='git --no-pager log --pretty=oneline --abbrev-commit --max-count=20'
+alias gl="git --no-pager log --pretty=format:'%C(bold)%h %C(magenta)%ae %C(cyan)%s%C(reset)' --abbrev-commit --max-count=20; echo"
+alias glt='git --no-pager log --pretty=oneline --abbrev-commit --max-count=20'
 alias gk="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
 
 function fix {
