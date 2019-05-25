90d4ea2489dfa9e05ca7c18789596159c16b2013
Peter McGarrigle <mcgarrigle@github.com>
2019-05-25 12:20:37 +0000

    added more packages

--- movein.sh
+++ movein.sh
@@ -3,12 +3,11 @@
 # curl -L bit.ly/shellin | bash
 
 yum install -y epel-release
-yum install -y vim git tmux
+yum install -y vim git tmux bash-completion yum-utils
 
 git clone 'https://github.com/mcgarrigle/bin.git'
 
-echo -e '\n. bin/bashrc\n' >> ~/.bashrc
+echo -e '\n. $HOME/bin/bashrc\n' >> ~/.bashrc
 
 ln -s bin/vimrc     .vimrc
 ln -s bin/tmux.conf .tmux.conf
-
