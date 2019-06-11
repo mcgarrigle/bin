12deee3caa8208427498ca3948274dea044c1679
McGarrigle, P, Peter <peter.mcgarrigle@bt.com>
2019-06-11 12:07:29 +0100

    added de docker alias

--- aliases-docker.sh
+++ aliases-docker.sh
@@ -5,5 +5,10 @@ alias dn="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{
 
 function dr {
   image=$1; shift
-  docker run -it --rm --mount type=bind,source=/root,target=/root "$image" $@
+  docker run -it --rm --mount type=bind,source=$HOME,target=/root "$image" $@
+}
+
+function de {
+  container="$2"
+  docker exec -it $1 ${container:=bash}
 }
