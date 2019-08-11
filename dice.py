47ef6cd36e4f184f519b8c2da9691fc3f179d7f0
Peter McGarrigle <mcgarrigle@github.com>
2019-08-12 00:40:40 +0100

    python dice

---
+++ dice.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys, os.path
+import random
+import argparse
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
+def roll():
+    digits = [ str(random.randint(1, 6)) for i in range(5) ]
+    return ''.join(digits)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-w", "--windows", help="Make complient with typical Windows policy", action="store_true")
+args = parser.parse_args()
+
+with open(os.path.join(ROOT, 'wordlist.txt')) as f:
+    text = f.read()
+    items = [ tuple(s.split()) for s in text.splitlines() ]
+    index = dict(items)
+
+words = [ (index[roll()], i) for i in range(4) ]
+
+if args.windows:
+    phrase = [ w[0].capitalize() + str(w[1]) for w in words ]
+    phrase = '.'.join(phrase)
+else:
+    phrase = [ w[0] for w in words ]
+    phrase = ' '.join(phrase)
+
+print(phrase)
