9a198ab763a926d024f0e7d8811bbdbe57d59f97
Peter McGarrigle <mcgarrigle@github.com>
2019-08-12 16:26:34 +0100

    tweaks

--- dice.py
+++ dice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os.path
+import os.path
 import random
 import argparse
 
@@ -10,19 +10,18 @@ def roll():
     digits = [ str(random.randint(1, 6)) for i in range(5) ]
     return ''.join(digits)
 
-def words():
+def load_words():
     with open(os.path.join(ROOT, 'wordlist.txt')) as f:
         text = f.read()
     items = [ tuple(s.split()) for s in text.splitlines() ]
-    index = dict(items)
-    return index
+    return dict(items)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-w", "--windows", help="Make complient with typical Windows policy", action="store_true")
 args = parser.parse_args()
 
-index = words()
-words = [ (index[roll()], i) for i in range(4) ]
+dicewords = load_words()
+words = [ (dicewords[roll()], i) for i in range(4) ]
 
 if args.windows:
     phrase = [ w[0].capitalize() + str(w[1]) for w in words ]
