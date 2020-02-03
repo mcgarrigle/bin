21761eb995cbc8e0917ff98affa823e0c8c0ad70
Peter McGarrigle <mcgarrigle@github.com>
2020-02-03 07:10:13 +0000

    gll is gl without limit

--- aliases-git.sh
+++ aliases-git.sh
@@ -11,6 +11,7 @@ alias co='git checkout'
 alias gd='git diff'
 alias gr='git remote -v'
 alias gl="git --no-pager log --pretty=format:'%C(bold)%h %ci %C(magenta)%ae %C(cyan)%s%C(reset)' --abbrev-commit --max-count=20; echo"
+alias gll="git --no-pager log --pretty=format:'%C(bold)%h %ci %C(magenta)%ae %C(cyan)%s%C(reset)' --abbrev-commit; echo"
 alias glt='git --no-pager log --pretty=oneline --abbrev-commit --max-count=20'
 alias gk="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
 
