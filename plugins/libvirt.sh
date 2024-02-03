7f14ab024a9868294759ff2257bd975adcd8bd95
Peter McGarrigle <mcgarrigle@github>
2024-02-03 17:16:26 +0000

    hypervisor command to set LIBVIRT_DEFAULT_URI

--- plugins/libvirt.sh
+++ plugins/libvirt.sh
@@ -1,13 +1,26 @@
-# ---------------------------------------------------
-# allow console to complete lists of running domains
-#
+# export LIBVIRT_DEFAULT_URI='qemu:///system'
 
-export LIBVIRT_DEFAULT_URI='qemu:///system'
+# -------------------------------------
+# hypervisor management
+
+# https://docs.virtuozzo.com/libvirt-docs-5.6.0/html/uri.html
+
+function hypervisor {
+  export LIBVIRT_DEFAULT_URI="$1"
+}
+
+hypervisor "default"
+
+# -------------------------------------
 
 console()
 {
   virsh console --domain "$1"
 }
+
+# ---------------------------------------------------
+# allow console to complete lists of running domains
+#
 
 _console_completions()
 {
