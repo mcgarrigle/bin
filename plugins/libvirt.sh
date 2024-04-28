cc1096c6811d3058f66bd1b2556e643d6624735c
Peter McGarrigle <mcgarrigle@github.com>
2024-04-28 11:26:50 +0000

    make management of LIBVIRT_DEFAULT_URI consistent with terraform provider

--- plugins/libvirt.sh
+++ plugins/libvirt.sh
@@ -5,11 +5,40 @@
 
 # https://docs.virtuozzo.com/libvirt-docs-5.6.0/html/uri.html
 
-function hypervisor {
-  export LIBVIRT_DEFAULT_URI="$1"
+declare -A HYPERVISORS
+
+HYPERVISORS[default]="qemu:///system"
+# HYPERVISORS[smol]="qemu+ssh://${USER}@smol.mac.wales/system?keyfile=${HOME}/.ssh/id_rsa&sshauth=privkey&no_verify=1"
+# HYPERVISORS[wee]="qemu+ssh://${USER}@wee.mac.wales/system?keyfile=${HOME}/.ssh/id_rsa&sshauth=privkey&no_verify=1"
+
+function _list_hypervisors {
+  local LIST=$(echo ${!HYPERVISORS[@]} | tr ' ' '\n' | sort)
+  echo "current: $HYPERVISOR => $LIBVIRT_DEFAULT_URI"
+  echo
+  for KEY in ${LIST}; do
+    echo "${KEY} => ${HYPERVISORS[$KEY]}"
+  done
 }
 
-hypervisor "default"
+function _set_hypervisor {
+  if [ -z ${HYPERVISORS[$1]} ]; then
+    echo "$1 not known"
+  else
+    export HYPERVISOR="$1"
+    export LIBVIRT_DEFAULT_URI=${HYPERVISORS[$1]}
+  fi
+}
+
+function hypervisor {
+  if [ -z "$1" ]; then
+    _list_hypervisors
+  else
+    _set_hypervisor "$1"
+    echo "$HYPERVISOR => $LIBVIRT_DEFAULT_URI"
+  fi
+}
+
+_set_hypervisor "default"
 
 # -------------------------------------
 
