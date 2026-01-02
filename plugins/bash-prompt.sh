439c15517fa035d6412879362ad0a04128fa98d9
Peter McGarrigle <mcgarrigle@github>
2026-01-02 11:59:23 +0000

    zsh support

--- plugins/bash-prompt.sh
+++ plugins/bash-prompt.sh
@@ -2,26 +2,22 @@ SOH="\001"
 STX="\002"
 
 function colour {
-  echo -e "${SOH}\e[$1m${STX}"
+  if [ "${SHELL}" = "zsh" ]; then
+    echo -e "%F{$1}"
+  else
+    echo -e "${SOH}\e[$2m${STX}"
+  fi
 }
 
-RESET="$(colour 39)"
-BLACK="$(colour 30)"
-RED="$(colour 31)"
-GREEN="$(colour 32)"
-YELLOW="$(colour 33)"
-BLUE="$(colour 34)"
-MAGENTA="$(colour 35)"
-CYAN="$(colour 36)"
-LIGHT_GRAY="$(colour 37)"
-DARK_GRAY="$(colour 90)"
-LIGHT_RED="$(colour 91)"
-LIGHT_GREEN="$(colour 92)"
-LIGHT_YELLOW="$(colour 93)"
-LIGHT_BLUE="$(colour 94)"
-LIGHT_MAGENTA="$(colour 95)"
-LIGHT_CYAN="$(colour 96)"
-WHITE="$(colour 97)"
+RESET="$(colour reset 39)"
+BLACK="$(colour black 30)"
+RED="$(colour red 31)"
+GREEN="$(colour green 32)"
+YELLOW="$(colour yellow 33)"
+BLUE="$(colour blue 34)"
+MAGENTA="$(colour magenta 35)"
+CYAN="$(colour cyan 36)"
+WHITE="$(colour white 37)"
 
 function utf8 {
   printf "%b" "\u$1"
@@ -33,10 +29,19 @@ BOX_HORIZONTAL="$(utf8 2500)"
 BOX_VERTICAL="$(utf8 2502)"
 BOX_LEFT_TEE="$(utf8 251C)"
 
-. /etc/os-release
+OS=$(uname)
+
+if [ "${OS}" = "Linux" ]; then
+  . /etc/os-release
+  OS=${ID}
+fi
+
+if [ "${OS}" = "Darwin" ]; then
+  SHELL="zsh"
+fi
 
 function _distro_icon {
-  case "${ID}" in
+  case "${OS}" in
 
     alpine)   echo -n ${CYAN}$(utf8 f300) ;;
 
@@ -44,15 +49,17 @@ function _distro_icon {
 
     rocky)    echo -n ${GREEN}$(utf8 f32b) ;;
 
+    Darwin)   echo -n ${WHITE}$(utf8 f302) ;;
+
     *)        echo -n ${YELLOW}$(utf8 f31a) ;;
 
   esac
-  echo ${RESET}
+  echo -n ${RESET}
 }
 
 DISTRO_ICON=$(_distro_icon)
 
-function _prompt_string {
+function _bash_prompt_string {
   BRANCH="$(__git_branch)"
   echo -n "${BOX_DOWN_LEFT}${BOX_HORIZONTAL} "
   echo -n "${GREEN}\\u${RESET} "
@@ -66,5 +73,24 @@ function _prompt_string {
   echo -n "${BOX_UP_RIGHT}${BOX_HORIZONTAL} "
   echo -n "\\$ "  
 }
+
+function _zsh_prompt_string {
+  BRANCH="$(__git_branch)"
+  echo -n "${BOX_DOWN_LEFT}${BOX_HORIZONTAL} "
+  echo -n "%F{green}%n%F{reset} "
+  echo -n "[${DISTRO_ICON} %m] "
+  echo -n "%T "
+  echo -n "%F{yellow}%~ "
+  echo -n "%F{cyan}${BRANCH}%F{reset}"
+  echo -n "\\r\\n"
+  echo -n "${BOX_VERTICAL}"
+  echo -n "\\r\\n"
+  echo -n "${BOX_UP_RIGHT}${BOX_HORIZONTAL} "
+  echo -n "%# "  
+}
         
-PROMPT_COMMAND='PS1=$(_prompt_string)'
+PROMPT_COMMAND='PS1=$(_bash_prompt_string)'
+
+function precmd {
+  PROMPT=$(_zsh_prompt_string)
+}
